36803728577ebeb8e0636cbb05892606361977c6
Brady Paterson <brady@odicro.com>
2020-02-01 02:38:06 -0500

    update
    - this file is currently a mess and unstable. there needs to be fixes to the upstream slack go-client.
    - initial Slack Block Message styling
    - target master, mod vendor; probably dont need when upstream is fixed

--- pkg/xkcd/xkcd.go
+++ pkg/xkcd/xkcd.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	//"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/nlopes/slack"
@@ -90,13 +91,116 @@ func sendRandomComic(rLoc string) {
 		hpath = prodPath
 	}
 
-	url := "https://hooks.slack.com/" + hpath
+	
+	//url := "https://hooks.slack.com/" + hpath
 	// setup post to be title + url
-	post := comic.Title + " " + comic.Img
+	//post := comic.Title + " " + comic.Img
+
+	// Use Block API
+	//post := SlackBlock(comic)
+	// /// //// ///
+	// Header Section
+	headerText := slack.NewTextBlockObject("mrkdwn", comic.Title, false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	// Actual post
+	/*
+		comicImg := slack.NewImageBlock(comic.Img, "some profile", "image-block", headerText)
+		//comicImg := slack.NewImageBlockElement(comic.Img, "test text")
+		imgSection := slack.NewSectionBlock(comicImg, nil, nil)
+	*/
+
+	comicImg := slack.NewTextBlockObject("mrkdwn", comic.Img, false, false)
+	imgSection := slack.NewSectionBlock(comicImg, nil, nil)
+
+	// Divider
+	divSection := slack.NewDividerBlock()
+
+	// Alt Text
+	aText := slack.NewTextBlockObject("mrkdwn", comic.Alt, false, false)
+	altTextSection := slack.NewSectionBlock(aText, nil, nil)
+
+	// Build Message with blocks created above
+	msg := slack.NewBlockMessage(
+		headerSection,
+		imgSection,
+		divSection,
+		altTextSection,
+	)
+
+	b, err := json.MarshalIndent(msg, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
+
+	//post := string(b)
+
+	/*
+	p := []byte(post)
+	var s struct{}
+	json.Unmarshal(p, &s)
+	fmt.Println("unmarshalled Body : ", string(p))
+
+	fix := []string{post}
+	i := 0
+	for x := 0; x < 2; x++ {
+		// remove the element at index i from fix
+		fix[i] = fix[len(fix)-1] // copy last element to index i
+		fix[len(fix)-1] = ""     // erase last element
+		fix = fix[:len(fix)-1]   // truncate slice
+	}
+
+	jString := strings.Join(fix, " ")
+	fmt.Println("unmarshalled 'fix' : ", string(jString))
+	*/
+	/// /// /// /// /// ///
+/*
 	payload := &slack.WebhookMessage{
-		Text:    string(post),
+		Text:    string(jString),
 		Channel: "#testing-zone",
 	}
 
 	slack.PostWebhook(url, payload)
+	*/
 }
+
+/*
+// SlackBlock formats an interactive message
+func SlackBlock(c XKCD_COMIC) string {
+	// Header Section
+	headerText := slack.NewTextBlockObject("mrkdwn", c.Title, false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	// Actual post
+	comicImg := slack.NewImageBlockElement(c.Img, "stub text")
+	imgSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	// Divider
+	divSection := slack.NewDividerBlock()
+
+	// Alt Text
+	aText := slack.NewTextBlockObject("mrkdwn", c.Alt, false, false)
+	altTextSection := slack.NewSectionBlock(aText, nil, nil)
+
+	// Build Message with blocks created above
+	msg := slack.NewBlockMessage(
+		headerSection,
+		imgSection,
+		divSection,
+		altTextSection,
+	)
+
+	b, err := json.MarshalIndent(msg, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
+
+	return string(b)
+}
+*/
